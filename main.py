9a20e7feff24bde1becb841bf01ddc0ea9fc7ca0
Valentin <vyotov07@gmail.com>
2026-07-21 08:58:44 +0300

    add ChatBot class

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ load_dotenv()
 
 api_key = os.getenv("api_key")
 
-client = OpenAI(api_key=api_key)
-history = []
-history.append({"role":"system","content":"You are really helpful assistant."})
-def chat(prompt):
-    history.append({"role": "user", "content": prompt})
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=history,
-    )
+class ChatBot:
+    def __init__(self):
+        self.history = [{"role":"system","content":"You are really helpful assistant."}]
+        self.client = OpenAI(api_key=api_key)
 
-    return response.choices[0].message.content.strip()
+    def chat(self,prompt):
+        self.history.append({"role": "user", "content": prompt})
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=self.history,
+        )
+        return response.choices[0].message.content.strip()
+
+
 
 if __name__ == "__main__":
+    chatbot = ChatBot()
     while True:
-        user = input("You: ")
-        if user.lower() in ["quit", "exit"]:
+        user_question = input("You: ")
+        if user_question.lower() in ["quit", "exit"]:
             break
-        response = chat(user)
-        history.append({"role": "assistant", "content": response})
+        response = chatbot.chat(user_question)
         print(f"Bot: {response}")
